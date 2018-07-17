53ed7e1215a6517a0fb8e22c9ae034ffc0df0423
Xiong-qi <1224571102@qq.com>
2018-07-17 18:16:10 +0800

    提交搭建

---
+++ secuitypanertdemo/securitydemo/src/main/java/com/xiongqi/securitydemo/SecuritydemoApplication.java
@@ -0,0 +1,20 @@
+package com.xiongqi.securitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SecuritydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritydemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring security";
+    }
+}
